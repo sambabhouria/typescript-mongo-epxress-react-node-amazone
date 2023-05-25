13dbb6d5d828ac1baa15adab9272e78eecf7ad0f
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-25 19:46:55 +0200

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
-import { sampleProducts } from './data'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header>
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand>Ecommerce App</Navbar.Brand>
+            <Navbar.Brand>ecommerce app</Navbar.Brand>
           </Container>
           <Nav>
             <a href="/cart" className="nav-link">
@@ -21,19 +21,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
