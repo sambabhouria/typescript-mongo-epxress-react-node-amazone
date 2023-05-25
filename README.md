33219388632c40ca8a81f67a05a4b5fae7a2705c
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-25 18:46:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TypeScript MERN ECOMMERCE AMAZONE
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
