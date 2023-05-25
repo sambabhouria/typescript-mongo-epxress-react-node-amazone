4e96f7f756e3bfa059f4a95e8915f6e4cfa2bdae
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-25 21:01:58 +0200

    fetch data form backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,19 @@
+import cors from 'cors'
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
+
 const app = express()
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
 
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
 })
-
 const PORT = 4000
-
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
 })
