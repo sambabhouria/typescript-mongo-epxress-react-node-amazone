d37c78bfcba0504ff759ef4ec0aaf45c7e99648d
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-25 19:14:25 +0200

    lsit of products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
-      <header>TS Amazona</header>
-      <main></main>
+      <header>Type-Script App Like Amazone</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
