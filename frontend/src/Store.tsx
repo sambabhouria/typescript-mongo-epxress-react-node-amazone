6d8cdc39226f9a01e75f73f148d75bf63bbe4f43
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-26 17:40:39 +0200

    Remove-From-Cart

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -33,6 +33,7 @@ const initialState: AppState = {
 type Action =
   | { type: 'SWITCH_MODE' }
   | { type: 'CART_ADD_ITEM'; payload: CartItem }
+  | { type: 'CART_REMOVE_ITEM'; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
@@ -51,6 +52,13 @@ function reducer(state: AppState, action: Action): AppState {
 
       localStorage.setItem('cartItems', JSON.stringify(cartItems))
 
+      return { ...state, cart: { ...state.cart, cartItems } }
+    }
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item: CartItem) => item._id !== action.payload._id
+      )
+      localStorage.setItem('cartItems', JSON.stringify(cartItems))
       return { ...state, cart: { ...state.cart, cartItems } }
     }
 
