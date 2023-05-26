03080beb11a267517333271651ee733db9efdd47
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-26 11:25:27 +0200

    add rating product itmes

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { useEffect, useReducer } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import { ApiError } from '../types/ApiError'
 import { Product } from '../types/Product'
 import { getError } from '../utils'
+import ProductItem from '../components/ProductItem'
 
 type State = {
   products: Product[]
@@ -65,19 +65,13 @@ export default function HomePage() {
     <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <Row>
-      {products.map((product) => (
-        <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={'/product/' + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
-        </Col>
-      ))}
+      <Row>
+        {products.map((product) => (
+          <Col key={product.slug} sm={6} md={4} lg={3}>
+            <ProductItem product={product} />
+          </Col>
+        ))}
+      </Row>
     </Row>
   )
 }
