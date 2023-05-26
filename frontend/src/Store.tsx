388664e0693a1bd56fba0627d602865d402a9bd3
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-26 16:10:03 +0200

    add to caler fontionality

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,24 +1,59 @@
 import React from 'react'
+import { Cart, CartItem } from './types/Cart'
 
 type AppState = {
-  mode: string | null
+  mode: string
+  cart: Cart
 }
 
 const initialState: AppState = {
   mode: localStorage.getItem('mode')
-    ? localStorage.getItem('mode')
+    ? localStorage.getItem('mode') || '{}'
     : window.matchMedia &&
       window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light',
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems') || '{}')
+      : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress') || '{}')
+      : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod') || '{}'
+      : 'PayPal',
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0,
+  },
 }
 
-type Action = { type: 'SWITCH_MODE' }
+type Action =
+  | { type: 'SWITCH_MODE' }
+  | { type: 'CART_ADD_ITEM'; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case 'SWITCH_MODE':
-      return { mode: state.mode === 'dark' ? 'light' : 'dark' }
+      return { ...state, mode: state.mode === 'dark' ? 'light' : 'dark' }
+    case 'CART_ADD_ITEM': {
+      const newItem = action.payload
+      const existItem = state.cart.cartItems.find(
+        (item: CartItem) => item._id === newItem._id
+      )
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item: CartItem) =>
+            item._id === existItem._id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem]
+
+      localStorage.setItem('cartItems', JSON.stringify(cartItems))
+
+      return { ...state, cart: { ...state.cart, cartItems } }
+    }
+
     default:
       return state
   }
@@ -31,7 +66,6 @@ const Store = React.createContext({
   dispatch: defaultDispatch,
 })
 
-// function StoreProvider(props: React.PropsWithChildren<{}>) {
 function StoreProvider(props: React.PropsWithChildren<object>) {
   const [state, dispatch] = React.useReducer<React.Reducer<AppState, Action>>(
     reducer,
