f25be1b2f6e05d30a5129fe62f728d7eaa6ffce0
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-26 19:53:02 +0200

    singup page adde

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,8 @@ import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import CartPage from './pages/CartPage'
 import SigninPage from './pages/SigninPage'
+import SignupPage from './pages/SignupPage'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StoreProvider } from './Store'
@@ -25,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="product/:slug" element={<ProductPage />} />
       <Route path="cart" element={<CartPage />} />
       <Route path="signin" element={<SigninPage />} />
+      <Route path="signup" element={<SignupPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
