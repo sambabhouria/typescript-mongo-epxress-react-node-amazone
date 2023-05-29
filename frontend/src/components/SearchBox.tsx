66183c6d10081b15348b1d63cc9ac52a4e8342cb
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-29 20:27:25 +0200

    theme like amazon

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
@@ -14,15 +14,15 @@ export default function SearchBox() {
   }
 
   return (
-    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+    <Form className="flex-grow-1 d-flex me-auto" onSubmit={submitHandler}>
       <InputGroup>
         <FormControl
           type="text"
           name="q"
           id="q"
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="search products..."
-          aria-label="Search Products"
+          placeholder="Search Amazona"
+          aria-label="Search Amazona"
           aria-describedby="button-search"
         ></FormControl>
         <Button variant="outline-primary" type="submit" id="button-search">
